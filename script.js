f3829ec65fe52534a5b4b57694a610e953aa1014
Italo Bruno Costa Silva <90716035+IT4LOBRUNO@users.noreply.github.com>
2024-10-14 14:21:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 document.getElementById('btn-courses').addEventListener('click', function() {
-        redirecionar('courses/index.html');
+        redirecionar('courses\index.html');
     });
 
 document.getElementById('btn-login').addEventListener('click', function() {
-        redirecionar('login/index.html');
+        redirecionar('login\index.html');
     });
 });
